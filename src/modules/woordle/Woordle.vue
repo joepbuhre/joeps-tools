d3869d4af0e2591c186e1892cf316fda790a3446
devmerces <79695868+joepbuhre@users.noreply.github.com>
2022-07-03 00:28:35 +0200

    Added check for words

--- src/modules/woordle/Woordle.vue
+++ src/modules/woordle/Woordle.vue
@@ -1,37 +1,48 @@
 <template>
     <div class="md:px-44 px-4">
         <p class="text-center">
-            {{ alertText }}
+            <span v-if="alertText">{{ alertText }}</span>
+            <span v-else>Woordle</span>
         </p>
-        <div>
-            <div v-for="i in maxTries" class="flex gap-1 mt-2">
-                <div v-for="ind in letters" class="grow h-16 border border-gray rounded-md flex"
+        <div class="">
+            <div v-for="i in maxTries" class="flex gap-1 mt-2 justify-center">
+                <div v-for="ind in letters" class="h-12 w-12 border border-gray rounded-md flex"
                     :class="{ 'bg-green-500': (tries?.[i - 1]?.[ind - 1]?.correct === 1), 'bg-yellow-300': (tries?.[i - 1]?.[ind - 1]?.correct === 2), 'bg-gray-400': (tries?.[i - 1]?.[ind - 1]?.correct === 3) }">
-                    <span class="m-auto text-xl">{{ tries?.[i - 1]?.[ind - 1]?.letter }}</span>
+                    <span class="m-auto text-2xl capitalize">{{ tries?.[i - 1]?.[ind - 1]?.letter }}</span>
                 </div>
             </div>
         </div>
-        <div class="bg-slate-100 mb-24 mt-2">
-            <div class="grid grid-cols-2 gap-5 my-2 p-3">
-            <button class="bg-blue-100 rounded-md px-2 py-1" @click="getWords()">Get Word</button>
-            <button class="bg-blue-100 rounded-md px-2 py-1" @click="removeLetter()">backspace</button>
-        </div>
-            <div class="md:w-1/3">
-                <div v-for="row in keyboardLayout" class="text-center mt-2 flex justify-center gap-1">
-                    <button v-for="key in row" @click="addLetter(key)"
-                        class="border border-slate-400 w-8 py-2 rounded-md"
-                        :class="{ 'bg-green-500 border-green-500': (lettersCorrect[key] === 1), 'bg-yellow-300 border-yellow-300': (lettersCorrect[key] === 2), 'bg-gray-400': (lettersCorrect[key] === 3) }">
-                        {{ key }}
-                    </button>
+        <div class="h-[300px] mt-2 relative">
+            <div class="absolute bottom-10 bg-slate-100 ">
+                <div class="grid grid-cols-3 gap-5 my-2 p-3">
+                    <button class="bg-blue-100 rounded-md px-2 py-1" @click="getWords()">Get Word</button>
+                    <button class="bg-blue-100 rounded-md px-2 py-1" @click="showHint()">hint</button>
+                    <button class="bg-blue-100 rounded-md px-2 py-1" @click="removeLetter()">backspace</button>
                 </div>
-                <button class="fixed top-0 right-0 text-xs opacity-10" @click="showWord()">cheat</button>
+                <div class="md:w-1/3">
+                    <div v-for="row in keyboardLayout" class="text-center mt-2 flex justify-center gap-1">
+                        <button v-for="key in row" @click="addLetter(key)"
+                            class="border border-slate-400 w-8 py-2 rounded-md"
+                            :class="{ 'bg-green-500 border-green-500': (lettersCorrect[key] === 1), 'bg-yellow-300 border-yellow-300': (lettersCorrect[key] === 2), 'bg-gray-400': (lettersCorrect[key] === 3) }">
+                            {{ key }}
+                        </button>
+                    </div>
+                    <button class="fixed top-0 right-0 text-xs opacity-10" @click="showWord()">cheat</button>
+                </div>
             </div>
+        </div>
+    </div>
+    <div v-if="modalOpen" @click.self="modalOpen = false" class="fixed inset-0 flex bg-black bg-opacity-20 px-3">
+        <div class="m-auto p-3 bg-white rounded-md shadow-lg">
+            <div v-html="modalText"></div>
+            <button class="bg-blue-100 rounded-md px-2 py-1 block m-auto" @click="modalOpen = false">Sluit</button>
+
         </div>
     </div>
 </template>
 
 <script setup>
-import { onMounted, ref } from 'vue';
+import { onMounted, ref, watch } from 'vue';
 import axios from "axios";
 
 const words = ref([])
@@ -88,6 +99,11 @@ const fullCorrect = ref(false)
 
 const lettersCorrect = ref({})
 
+const wikiWord = ref(null)
+
+const modalText = ref('')
+const modalOpen = ref(false)
+
 const reset = _ => {
     fullCorrect.value = false
     inputValue.value = ''
@@ -126,8 +142,8 @@ const setLetterArray = _ => {
 }
 
 const checkLetters = _ => {
-    if (!words.value.includes(inputValue.value) && !(inputValue.value.substring(0,3) === 'qqq')) {
-        addAlert(`this isn't a word`)
+    if (!words.value.includes(inputValue.value) && !(inputValue.value.substring(0, 3) === 'qqq')) {
+        addAlert(`Dit is geen woord, ofwel maar helaas voor jou`)
         return false
     }
 
@@ -135,9 +151,10 @@ const checkLetters = _ => {
     const corrWord = Array.from(word.value)
 
     let checkedLetters = []
-        
+
 
     letters.forEach((letter, index) => {
+
         if (letter === corrWord[index]) {
             tries.value[atTry.value][index].correct = 1
             lettersCorrect.value[letter] = 1
@@ -145,7 +162,11 @@ const checkLetters = _ => {
             checkedLetters.push(letter)
 
             tries.value[atTry.value][index].correct = 2
-            lettersCorrect.value[letter] = 2
+
+            if (lettersCorrect?.value?.[letter] !== 1) {
+                lettersCorrect.value[letter] = 2
+            }
+
         } else {
             tries.value[atTry.value][index].correct = 3
             lettersCorrect.value[letter] = 3
@@ -178,7 +199,37 @@ const checkLetters = _ => {
 
 onMounted(() => {
     setLetterArray()
+    getWords()
+    setWord()
 })
+
+watch(wikiWord, () => {
+
+    if (wikiWord.value?.extract === undefined) {
+        setWord()
+        return false
+    }
+
+    if ('missing' in wikiWord.value) {
+        setWord()
+        return false
+    }
+
+    if (wikiWord.value?.extract.match(/\'/gi)) {
+        setWord()
+    }
+
+    if (wikiWord.value?.extract.match(/(kan verwijzen naar)/gi)) {
+        setWord()
+        return false
+    }
+})
+
+const showHint = _ => {
+    const reg = new RegExp(word.value, 'gi')
+    modalText.value = wikiWord.value?.extract.replace(reg, `<span class="blur-text">${word.value}</span>`)
+    modalOpen.value = true
+}
 
 const setLetters = _ => {
     console.log(Array.from(inputValue.value))
@@ -192,7 +243,7 @@ const setLetters = _ => {
 }
 
 const addLetter = letter => {
-    if(inputValue.value.length === letters.value) {
+    if (inputValue.value.length === letters.value) {
         return false
     }
 
@@ -212,10 +263,12 @@ const removeLetter = _ => {
 }
 
 const setWord = () => {
+    addAlert('Aan het zoeken voor een woord')
     reset()
     const rand = Math.round(Math.random() * words.value.length)
-    console.log(rand)
     word.value = words.value[rand]
+    checkWord()
+
 }
 
 const getWords = _ => {
@@ -228,5 +281,31 @@ const getWords = _ => {
         })
 }
 
+const checkWord = _ => {
+    const url = new URL('https://nl.wikipedia.org/w/api.php')
+    const search = new URLSearchParams({
+        action: "query",
+        prop: "extracts",
+        titles: word.value,
+        format: "json",
+        origin: "*",
+        redirects: null,
+        exsentences: 2,
+        explaintext: null
+    })
+    url.search = search.toString()
+    axios.get(url.toString())
+        .then(res => {
+            wikiWord.value = Object.values(res?.data?.query?.pages)?.[0]
+        })
+
+}
+
 </script>
 
+<style>
+.blur-text {
+    text-shadow: 0 0 8px black;
+    color: transparent;
+}
+</style>
